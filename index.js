ac3562196125286b7e2f41c44e32827842651344
Sreejith <59050790+Sreejithinfo@users.noreply.github.com>
2021-05-27 17:26:12 +0530

    Fix/msg (#4)
    * files added.
    
    * a added
    
    * message added

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ console.log('file3');
 console.log('number1');
 console.log('number2');
 console.log('number3');
+
+console.log('s1');
+console.log('s2');
+console.log('s3');
+
+console.log('s1');
+console.log('s2');
+console.log('s3');
+console.log('s4');
